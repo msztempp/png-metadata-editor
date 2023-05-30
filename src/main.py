be0a16bc207ecc9f7cc031e2d4f3583e1d665ebf
mszkudla <259248@student.pwr.edu.pl>
2023-05-30 22:15:38 +0200

    add TODO list of chunks

--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
-from menu import menu
+from menu import Menu
+
+# https://www.w3.org/TR/2003/REC-PNG-20031110/#5Chunk-layout
+
+
+# TODO:
+# Critical:
+# IHDR PLTE IDAT IEND
+# Anicillary:
+# tEXT
+# zamiast tEXT mozna iTXT, serializowany xmp
+# sprawdzenie czy jest, jesli nie ( klucz, wartosc )
+# jeesli jest xmp, pobieral xml -> formatuje i wpisuje na konsole
+# PLTE czy jest roznie przetrwarzana, zaleznie od pliku bit_deph and color type
+# GAMA
+# tIME
+# sRGB
+# tRNS
+
+
 if __name__ == "__main__":
-    menu()
+    menu = Menu()
+    menu.start()
