6723dab166a0e4a6427c6e40e7eb60fc63dcad84
mszkudla <259248@student.pwr.edu.pl>
2023-06-04 21:41:17 +0200

    refactor chunk class

--- src/chunk.py
+++ src/chunk.py
@@ -1,14 +1,34 @@
 class Chunk:
-    def __init__(self, length, chunk_type, crc):
-        self.length = length
-        self.chunk_type = chunk_type
-        self.crc = crc
+    def __init__(self, chunk_bytes=None, chunk_list=None):
+
+        self.crc = None
+        self.data = None
+        self.chunk_type = None
+        self.length = None
+
+        if chunk_bytes and not chunk_list:
+            self.single_chunk_init(chunk_bytes)
+        elif not chunk_bytes and chunk_list:
+            self.multiple_chunk_init(chunk_list)
+        else:
+            raise Exception('Invalid input')
+
+    def single_chunk_init(self, chunk_bytes):
+        self.length = int.from_bytes(chunk_bytes[:4], 'big')
+        self.chunk_type = chunk_bytes[4:8].decode('utf-8')
+        self.data = chunk_bytes[8:8 + self.length]
+        self.crc = chunk_bytes[-4:]
+
+    def multiple_chunk_init(self, chunk_list):
+        self.length = [chunk.length for chunk in chunk_list]
+        self.chunk_type = chunk_list[0].chunk_type
+        self.data = [chunk.data for chunk in chunk_list]
+        self.crc = [chunk.crc for chunk in chunk_list]
 
     def print_basic_info(self):
-        print("Type: {type}".format(type=self.chunk_type))
-        print("Length: {length} bytes".format(length=self.length))
-        print("CRC: {crc}".format(crc=self.crc))
+        print('Type:', self.chunk_type)
+        print('Length:', self.length, 'chunk_bytes')
+        print('CRC:', self.crc)
 
-    # TODO: implement this
     def show_details(self):
-        print("To be implemented\n")
+        print('To be implemented\n')
