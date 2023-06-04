d008b3646007761d74aa93c0913e2b3d640df72f
mszkudla <259248@student.pwr.edu.pl>
2023-06-04 21:54:03 +0200

    add handling transparency and palette to idat chunk

--- src/idat.py
+++ src/idat.py
@@ -3,16 +3,16 @@ import zlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-# https://pyokagan.name/blog/2019-10-14-png/
 
-BYTES_PER_PIXEL = {
-    0: 1,
-    2: 3,
-    3: 1,
-    4: 2,
-    6: 4,
-    'default': 'Not found'
-}
+def byte_per_pixel(color_type):
+    switcher = {
+        0: 1,
+        2: 3,
+        3: 1,
+        4: 2,
+        6: 4,
+    }
+    return switcher.get(color_type, 'Not found')
 
 
 def paeth_predictor(a, b, c):
@@ -30,15 +30,16 @@ def paeth_predictor(a, b, c):
 
 
 class IDAT(Chunk):
-    def __init__(self, length, data, crc, width, height, color_type, palette=None):
-        super().__init__(length, "IDAT", crc)
-        self.palette = palette
+    def __init__(self, init_data, width, height, color_type):
         self.recon_data = None
-        self.data = data
+        super().__init__(chunk_list=init_data) if isinstance(init_data, list) else super().__init__(chunk_bytes=init_data)
+        if isinstance(self.data, list):
+            self.length = sum(self.length)
+            self.data = b''.join(self.data)
         self.width = width
         self.height = height
         self.color_type = color_type
-        self.bytes_per_pixel = BYTES_PER_PIXEL.get(color_type)
+        self.bytes_per_pixel = byte_per_pixel(color_type)
         self.analyse()
 
     def analyse(self):
@@ -67,7 +68,7 @@ class IDAT(Chunk):
                         self.recon_c(r, c, stride)
                     )
                 else:
-                    raise Exception("Unknown filter type: " + str(filter_type))
+                    raise Exception('Unknown filter type: ' + str(filter_type))
                 self.recon_data.append(recon_x & 0xff)  # truncation to byte
 
     def recon_a(self, r, c, stride):
@@ -79,13 +80,40 @@ class IDAT(Chunk):
     def recon_c(self, r, c, stride):
         return self.recon_data[(r - 1) * stride + c - self.bytes_per_pixel] if r > 0 and c >= self.bytes_per_pixel else 0
 
+    def display_data(self, title, data=None, bytes_per_pixel=None):
+        if data is None:
+            data = self.recon_data
+        fig, ax = plt.subplots(1, 1)
+        if bytes_per_pixel is None:
+            bytes_per_pixel = self.bytes_per_pixel
+        if bytes_per_pixel == 1:
+            ax.imshow(np.array(data).reshape((self.height, self.width)), cmap='gray')
+        else:
+            ax.imshow(np.array(data).reshape((self.height, self.width, bytes_per_pixel)))
+        ax.set_axis_off()
+        ax.set_facecolor('whitesmoke')
+        fig.patch.set_facecolor('whitesmoke')
+        fig.tight_layout()
+        fig.canvas.set_window_title(title)
+        plt.draw()
+        plt.pause(0.001)
+
     def apply_palette(self, palette):
-        self.recon_data = [pixel for pixel_i in self.recon_data for pixel in palette[pixel_i]]
-        self.bytes_per_pixel = 3
+        new_data = [pixel for pixel_i in self.recon_data for pixel in palette[pixel_i]]
+        self.display_data('IDAT + palettes', data=new_data, bytes_per_pixel=3)
 
-    def print_info(self):
+    def apply_transparency(self, transparency_data, palette=None):
+        if palette is not None:
+            transparent_palette = []
+            for i in range(len(palette)):
+                if i > len(transparency_data) - 1:
+                    transparent_palette.append((palette[i][0], palette[i][1], palette[i][2], 255))
+                else:
+                    transparent_palette.append((palette[i][0], palette[i][1], palette[i][2], transparency_data[i]))
+            new_data = [pixel for pixel_i in self.recon_data for pixel in transparent_palette[pixel_i]]
+            self.display_data('IDAT + palette + transparency', data=new_data, bytes_per_pixel=4)
+        print('palette and transparency applied to IDAT')
+
+    def details(self):
         self.print_basic_info()
-
-    def show_details(self):
-        plt.imshow(np.array(self.recon_data).reshape((self.height, self.width, self.bytes_per_pixel)))
-        plt.show()
+        self.display_data('IDAT')
