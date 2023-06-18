2310c4c4444631e6e78acf8fd7351cdcb9c61bc1
mszkudla <259248@student.pwr.edu.pl>
2023-06-18 16:16:21 +0200

    Add time chunk

--- src/chunks/anicillary/chrm.py
+++ src/chunks/anicillary/chrm.py
@@ -4,7 +4,7 @@ from src.chunk import Chunk
 class CHRM(Chunk):
     def __init__(self, chunk_bytes):
         super().__init__(chunk_bytes)
-        self.whitePointX=None
+        self.whitePointX = None
         self.whitePointY = None
         self.redX = None
         self.redY = None
@@ -15,18 +15,18 @@ class CHRM(Chunk):
         self.analyse()
 
     def analyse(self):
-        if self.length!=32:
+        if self.length != 32:
             raise Exception('CHRM chunk length is invalid')
 
-        data=self.data
-        self.whitePointX=int.from_bytes(data[0:4], byteorder='big')/100000
-        self.whitePointY = int.from_bytes(data[4:8], byteorder='big')/100000
-        self.redX = int.from_bytes(data[8:12], byteorder='big')/100000
-        self.redY = int.from_bytes(data[12:16], byteorder='big')/100000
-        self.greenX = int.from_bytes(data[16:20], byteorder='big')/100000
-        self.greenY = int.from_bytes(data[20:24], byteorder='big')/100000
-        self.blueX = int.from_bytes(data[24:28], byteorder='big')/100000
-        self.blueY = int.from_bytes(data[28:32], byteorder='big')/100000
+        data = self.data
+        self.whitePointX = int.from_bytes(data[0:4], byteorder='big') / 100000
+        self.whitePointY = int.from_bytes(data[4:8], byteorder='big') / 100000
+        self.redX = int.from_bytes(data[8:12], byteorder='big') / 100000
+        self.redY = int.from_bytes(data[12:16], byteorder='big') / 100000
+        self.greenX = int.from_bytes(data[16:20], byteorder='big') / 100000
+        self.greenY = int.from_bytes(data[20:24], byteorder='big') / 100000
+        self.blueX = int.from_bytes(data[24:28], byteorder='big') / 100000
+        self.blueY = int.from_bytes(data[28:32], byteorder='big') / 100000
 
     def details(self):
         self.print_basic_info()
